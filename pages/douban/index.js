b4ed9384b7b371fb25214cbc94a2e8da99aab401
Javy <javyho@163.com>
2020-07-23 10:36:51 +0800

    2.7 实现下拉刷新

--- pages/douban/index.js
+++ pages/douban/index.js
@@ -57,7 +57,15 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    wx.showLoading({
+      title: '加载中',
+    })
+    
+    this.retrieveData().then(() => wx.stopPullDownRefresh({
+      success: (res) => {
+        wx.hideLoading()
+      },
+    }))
   },
 
   /**
